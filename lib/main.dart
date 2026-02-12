a67ad9a070042a7b1ba20f67925ce210a21c3a10
Jarred Myers <jarred@Mac.attlocal.net>
2026-02-12 08:22:53 -0600

    Changed titleLarge text but kept global font styling

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,32 @@ var kColorScheme = ColorScheme.fromSeed(
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(fontFamily: 'SpaceMono').copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: CardThemeData().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
+      theme:
+          ThemeData(
+            fontFamily: 'SpaceMono',
+            textTheme: TextTheme(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+          ).copyWith(
+            colorScheme: kColorScheme,
+            appBarTheme: AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer,
+            ),
+            cardTheme: CardThemeData().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer,
+              ),
+            ),
           ),
-        ),
-        // textTheme: ThemeData().textTheme.copyWith(
-        //   titleLarge: GoogleFonts.spaceMono(
-        //     fontWeight: FontWeight.bold,
-        //     color: kColorScheme.onSecondaryContainer,
-        //     fontSize: 14,
-        //   ),
-        // ),
-      ),
       home: Expenses(),
     ),
   );
